3466a7a4a9bdba19f1fc2675aaee4d582ab48960
redux9 <55329395+redux9@users.noreply.github.com>
2019-10-17 14:07:22 +0200

    little modifications

--- src/GestorFiguras.java
+++ src/GestorFiguras.java
@@ -4,7 +4,7 @@ public interface GestorFiguras {
 
     public static void sumaAreas(Figura[] l) {
         double areaTotal = 0;
-        for (Figura f : l) { //"por cada f que hay en l"
+        for (Figura f : l) { /**"por cada f que hay en l"*/
             areaTotal += f.getAreaFigura();
         }
         System.out.println("La suma de todas las areas es: " + areaTotal);
